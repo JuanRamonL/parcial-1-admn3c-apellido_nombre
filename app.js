485f15bd5cdd67522521f4ec0e31bde5c8d0eb2a
JuanRamonL <78512252+JuanRamonL@users.noreply.github.com>
2023-05-22 18:12:31 -0300

    Sesión, Ingreso a la app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let app = new Vue({
+const app = new Vue({
     el:'#contenedor',
     data:{
         texto:'hola',
@@ -6,13 +6,30 @@ let app = new Vue({
 
         login: true,
 
-
         link:[
-            { text: 'home', url:'/home', enable: true},  
-            { text: 'abaut', url:'/abaut', enable: false}, 
+            { text: 'home', url:'#/home', enable: true},  
+            { text: 'mi info', url:'#/abaut', enable: false},
+            { text: 'configuración', url:'#/configuración', enable: false},
+
         ]
+    },
 
 
-    }
+    methods: {
+        textochau(){
+            this.texto= 'chau'
+        },
+        
+        open(){
+            this.login=false
+        },
+        close(){
+            this.login= true
+        },
 
+        envio(){
+            console.log("se fue")
+        }
+    },
+        
   })

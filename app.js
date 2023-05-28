69067e8f8a59ffabcb2466725cb7f8f7ccf6e51d
JuanRamonL <78512252+JuanRamonL@users.noreply.github.com>
2023-05-28 16:29:51 -0300

    add: vue-router

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
+const home = {
+    template:`
+    <div id="contenedor">
+        <h1>hola mundo, estoy vivo y en home</h1>
+    </div>
+    `,
+};
+
+
+const mis_tareas = {
+    template:`
+        <mis-tareas></mis-tareas>
+
+    `,
+};
+
+/*
+    const configuración = {
+        template:`
+        <h1>hola mundo, y en configuracion</h1>`,
+    };
+*/
+
+
+
+const router = new VueRouter({
+    routes:
+    [
+        {path:'/home', component: home },
+        {path:'/mis_tareas', component: mis_tareas },
+        //{path:'/configuración', component: configuración },
+
+
+    ]
+});
+
+
+
 const app = new Vue({
     el:'#contenedor',
+    router,
     data:{
 
         //------------datos---------- 
@@ -7,13 +46,18 @@ const app = new Vue({
         login: false,
 
         link:[
-            { text: 'home', url:'#/home', enable: true},  
-            { text: 'mis tareas', url:'#/abaut', enable: false},
-            { text: 'configuración', url:'#/configuración', enable: false},
+            { text: 'home', url:'/home', enable: true},  
+            { text: 'mis tareas', url:'/mis_tareas', enable: false},
+            //{ text: 'configuración', url:'/configuración', enable: false},
 
         ],
         estadologin: false,
+
+    
     },
+
+    
+
 
      //metodo que vue ejecuta cuando está listo para mostrar la pagina 
     mounted() {
--- componentes/barra-de-navegacion.js
+++ componentes/barra-de-navegacion.js
@@ -1,25 +1,25 @@
 Vue.component('barra-de-navegacion', {
-    template: `
-      <nav class="navbar navbar-expand-lg navbar-light ">
-        <div class="container-fluid " id="bgColor">
-          <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo" width="50px"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"> </span>
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container-fluid " id="bgColor">
+        <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo" width="50px"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"> </span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 p-3 " >
+            <li v-for="item in link" class="nav-item" v-if="item.enable || login">
+              <router-link class="nav-link active" aria-current="page" :to="item.url">{{item.text}}</router-link>
+            </li>        
+          </ul>
+          <button type="submit" :class="'btn btn-outline-' + (login ? 'danger' : 'success')" @click="sesion">
+            {{ login ? 'Cerrar sesión' : 'Iniciar sesión'}}
           </button>
-          <div class="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 p-3 " >
-              <li v-for="item in link" class="nav-item" v-if="item.enable || login">
-                <a class="nav-link active" aria-current="page" href="#">{{item.text}}</a>
-              </li>        
-            </ul>
-            <button type="submit" :class="'btn btn-outline-' + (login ? 'danger' : 'success')" @click="sesion">
-              {{ login ? 'Cerrar sesión' : 'Iniciar sesión'}}
-            </button>
-          </div>
         </div>
-      </nav>
-    `,
-    props:[
-        'login','link','sesion',
-    ],
-  })
+      </div>
+    </nav>
+  `,
+  props: [
+    'login', 'link', 'sesion',
+  ],
+});
